e301327bd262fc53e344f2cabcc075cff05a8695
Joe Hamer <jonahliampeter@gmail.com>
2023-03-24 16:46:10 +0000

    changes made in redhat

--- nested-ui/gui.go
+++ nested-ui/gui.go
@@ -1,21 +1,33 @@
 package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
+
+type rectint struct {
+	x, y, w, h int32
+}
 type vec2int struct {
-	x,y int32
-}
-type frame struct {
-	x,y,w,h int32
-	
-	parent *frame
+	x, y int32
 }
 
+const (
+	topLeft   = 0
+	topCenter = 1
+	topRight  = 2
 
-func BeginWindow(width int32, height int32, title string) frame{
-	windowFrame := frame{x: 0, y: 0, w: width, h: height, parent: nil}
+	middleLeft   = 3
+	middleCenter = 4
+	middleRight  = 5
+
+	bottomLeft   = 6
+	bottomCenter = 7
+	bottomRight  = 8
+)
+
+func BeginWindow(width int32, height int32, title string) rectint {
+	windowFrame := rectint{0, 0, width, height}
 
 	rl.InitWindow(width, height, title)
-	
+
 	return windowFrame
 }
 
@@ -23,37 +35,86 @@ func EndWindow() {
 	rl.CloseWindow()
 }
 
-func BeginFrame(x int32, y int32, width int32, height int32, parent frame) frame{
-	newFrame := frame{
-		x: x, y: y, w: width, h: height,
+func BeginFrame(rect rectint, anchor int, parent rectint) rectint {
+	/*newFrame := frame{
+		rect:   rect,
 		parent: &parent,
-	}
-	real := getRelativePos(vec2int{x,y}, parent)
-	rl.DrawRectangleLines(real.x, real.y, width, height, rl.Black)
-	
-	return newFrame
+	}*/
+	newRect := rectint{w: rect.w, h: rect.h}
+
+	//DrawRectLines(rect, rl.Black, parent)
+
+	return newRect
 }
 
-func EndFrame() {
+/*
+func EndFrame(frame) {
 	currentFrame = *currentFrame.parent
-}
+}*/
 
-func DrawRect(x int32, y int32, width int32, height int32) {
-	real := getRelativePos(vec2int{x,y}, currentFrame)
-	rl.DrawRectangle(real.x, real.y, width, height, rl.Blue)
+func DrawRect(rect rectint, col rl.Color, parent rectint) {
+	DrawRectAnchored(rect, topLeft, col, parent)
 }
-
-func getRelativePos(v vec2int, f frame) vec2int{
+func DrawRectLines(rect rectint, col rl.Color, parent rectint) {
+	real := getRelativePos(rect, topLeft, parent)
+	rl.DrawRectangleLines(real.x, real.y, rect.w, rect.h, col)
+}
+func DrawRectAnchored(rect rectint, anchor int, col rl.Color, parent rectint) {
+	real := getRelativePos(rect, anchor, parent)
+	rl.DrawRectangle(real.x, real.y, rect.w, rect.h, col)
+} /*
+func getRelativePos(v vec2int, f frame) vec2int {
 	realX := v.x
 	realY := v.y
 	// if f.parent == nil {
 	// 	return vec2int{x:realX,y:realY}
 	// }
-	cf := f
-	for cf.parent != nil {
-		realX += cf.x
-		realY += cf.y
-		cf = *cf.parent
+	currentFrame := f
+	for currentFrame.parent != nil {
+		realX += currentFrame.rect.x
+		realY += currentFrame.rect.y
+		currentFrame = *currentFrame.parent
 	}
-	return vec2int{x:realX,y:realY}
+	return vec2int{x: realX, y: realY}
+}*/
+func getRelativePos(rect rectint, anchor int, parent rectint) rectint {
+	newRect := rectint{w: rect.w, h: rect.h}
+
+	// find the position of the rectangle as if it were at 0,0
+	// modify the rect position based on the anchor
+
+	switch a := anchor; a {
+	case topLeft:
+		newRect.x = rect.x
+		newRect.y = rect.y
+	case topCenter:
+		newRect.x = (parent.w-rect.w)/2 + rect.x
+		newRect.y = rect.y
+	case topRight:
+		newRect.x = (parent.w - rect.w) - rect.x
+		newRect.y = rect.y
+
+	case middleLeft:
+		newRect.x = rect.x
+		newRect.y = (parent.h-rect.h)/2 + rect.y
+	case middleCenter:
+		newRect.x = (parent.w-rect.w)/2 + rect.x
+		newRect.y = (parent.h-rect.h)/2 + rect.y
+	case middleRight:
+		newRect.x = (parent.w - rect.w) - rect.x
+		newRect.y = (parent.h-rect.h)/2 + rect.y
+
+	case bottomLeft:
+		newRect.x = rect.x
+		newRect.y = (parent.h - rect.h) - rect.y
+	case bottomCenter:
+		newRect.x = (parent.w-rect.w)/2 + rect.x
+		newRect.y = (parent.h - rect.h) - rect.y
+	case bottomRight:
+		newRect.x = (parent.w - rect.w) - rect.x
+		newRect.y = (parent.h - rect.h) - rect.y
+	}
+	newRect.x -= parent.x
+	newRect.y -= parent.y
+	return newRect
 }
--- nested-ui/main.go
+++ nested-ui/main.go
@@ -7,22 +7,29 @@ import (
 )
 
 func main() {
-	BeginWindow(800, 450, "raylib [core] example - basic")
+	rl.SetConfigFlags(rl.FlagWindowResizable)
+	rootFrame := BeginWindow(800, 450, "raylib [core] example - basic")
 	font := rl.LoadFont("../assets/regular.ttf")
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
+		if rl.IsWindowResized() {
+			rootFrame.rect.w = int32(rl.GetScreenWidth())
+			rootFrame.rect.h = int32(rl.GetScreenHeight())
+		}
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
-		BeginFrame(10, 10, 50, 50)
-		BeginFrame(5, 5, 10, 10)
-		DrawRect(0,0,20,20)
-		EndFrame()
-		EndFrame()
-		
-		rl.DrawTextEx(font,"sjdviov", rl.Vector2{X: 10, Y: 10}, 32, 0, rl.Black);
+		frameA := BeginFrame(rectint{10, 10, 50, 50}, rootFrame)
+		frameB := BeginFrame(rectint{5, 5, 10, 10}, frameA)
+		DrawRectAnchored(rectint{0, 0, 20, 20}, middleCenter, rl.Color{255, 0, 0, 128}, frameB)
+		DrawRectAnchored(rectint{0, 0, 20, 20}, middleCenter, rl.Color{255, 0, 0, 128}, frameA)
+		DrawRectAnchored(rectint{0, 0, 20, 20}, middleCenter, rl.Color{255, 0, 0, 128}, rootFrame)
+		//EndFrame()
+		//EndFrame()
+
+		rl.DrawTextEx(font, "sjdviov", rl.Vector2{X: 10, Y: 10}, 32, 0, rl.Black)
 		rl.EndDrawing()
 	}
 
